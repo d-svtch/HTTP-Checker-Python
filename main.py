26e20a8dd22ba29c44761ec42ae4b90d629c33f3
Svtch <61234128+d-svtch@users.noreply.github.com>
2022-11-16 21:26:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,64 @@
 import requests
 
-req = requests.get('<a href="http://www.edureka.co/">http://www.edureka.co/</a>')
+import time
+import logging
+import asyncio
 
-req.encoding  # returns 'utf-8'
-req.status_code  # returns 200
-req.elapsed  # returns datetime.timedelta(0, 1, 666890)
-req.url  # returns '<a href="https://edureka.co/">https://edureka.co/</a>'
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
-req.history
+TOKEN = "5644056150:AAHlW3abaYK5W3LlBAEFc2VoAQ27841qzjQ"
+url = 'https://zabbix.medvedev-it.ru/zabbix.php?action=dashboard.view'
 
-req.headers['Content-Type']
-# returns 'text/html; charset=utf-8'
+listner = False
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    user_id = message.from_user.id
+    await message.reply(f'Чо умею:\n/start - вызов меню (вот этого самого) \n/status - запросить статус в ручную \n/listner_switch - устанавливает или отключает уведомления со статусами')
+
+    while listner == True :
+        await asyncio.sleep(30*1*1)
+        await bot.send_message(user_id, 'Авто-Статус: ' + str(requests.get(url)))
+
+@dp.message_handler(commands=['listner_switch'])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    global listner
+    if flag == False:
+        flag = True
+        await bot.send_message(user_id, 'Авто-уведомления каждые 30 секунд - вкл')
+        while flag == True:
+            await asyncio.sleep(30 * 1 * 1)
+            await bot.send_message(user_id, 'Статус: ' + str(requests.get(url)))
+    elif flag == True:
+        flag = False
+        await bot.send_message(user_id, 'АТПИСКА')
+
+async def listner (id):
+    user_id = id
+    global listner
+
+    if flag == False:
+        flag = True
+        await bot.send_message(user_id, 'Авто-уведомления каждые 30 секунд - вкл')
+        while flag == True:
+            await asyncio.sleep(30 * 1 * 1)
+            await bot.send_message(user_id, 'Статус: ' + str(requests.get(url)))
+
+    elif flag == True:
+        flag = False
+        await bot.send_message(user_id, 'АТПИСКА')
+
+@dp.message_handler(commands=['status'])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    #logging.info(f'{user_id} {user_full_name} {time.asctime()}')
+    await bot.send_message(user_id, 'Статус: ' + str(requests.get(url)))
+
+executor.start_polling(dp)
